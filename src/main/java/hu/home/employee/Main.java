9ca881fdbe46b4eb03ac7c981154b056a16ed133
Szabolcs <szabolcs.beko@gmail.com>
2023-06-11 14:36:02 +0200

    John commit

--- src/main/java/hu/home/employee/Main.java
+++ src/main/java/hu/home/employee/Main.java
@@ -14,5 +14,6 @@ public class Main {
         System.out.println(db.findEmployee("Barbara"));
         System.out.println(db.findEmployee("Jil"));
         System.out.println(db.findEmployee("Jack"));
+        System.out.println(db.findEmployee("John"));
     }
 }
